7e38d2be8c58c0d34746bfb180272a1f65ec73c5
Raj Dhinge <rajkumar.dhinge.mca15@gmail.com>
2023-05-17 17:49:12 +0530

    :zap: Added meals item page

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,8 @@
 import React from "react";
 import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card";
+import MealItem from "./MealItem";
+
 const DUMMY_MEALS = [
 	{
 		id: 'm1',
@@ -27,11 +30,18 @@ const DUMMY_MEALS = [
 	},
 ];
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map(meals => <li>{meals.name}</li>);
+	const mealsList = DUMMY_MEALS.map(meals => <MealItem
+		key={meals.key}
+		name={meals.name}
+		description={meals.description}
+		price={meals.price}
+	/>);
 
 	return (
 		<section className={classes.meals}>
-			<ul>{mealsList}</ul>
+			<Card>
+				<ul>{mealsList}</ul>
+			</Card>
 		</section>
 	);
 }
